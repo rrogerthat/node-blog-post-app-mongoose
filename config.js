b28cf5decb405854a8211115ed60e97fad83bb7f
Roger Hwang <hroger19@gmail.com>
2018-05-16 19:12:11 -0700

    deployed to Heroku

--- config.js
+++ config.js
@@ -2,3 +2,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/NodeBlogPostsApp';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-NodeBlogPostsApp';
 exports.PORT = process.env.PORT || 8080;
+
+//app name on heroku: guarded-scrubland-84208
