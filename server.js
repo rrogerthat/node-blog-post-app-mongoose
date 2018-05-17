2592d148ea10c1057ba4028d83719d6265fdbcf1
Roger Hwang <hroger19@gmail.com>
2018-05-16 17:34:32 -0700

    tested Post request on Postman and passed

--- server.js
+++ server.js
@@ -36,6 +36,30 @@ app.get('/posts/:id', (req, res) => {
     	});
 });
 
+app.post('/posts', (req, res) => {
+
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  BlogPost
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author
+    })
+    .then(blogpost => res.status(201).json(blogpost.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
 
 let server;
 
